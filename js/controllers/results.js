14f7dbf4c4b52ad28a54b7536c2d761b505099e2
plopushin <lopushin@ualberta.ca>
2017-01-26 11:04:50 -0700

    Get results to display when quiz is finished -> still need to fix error where the quiz questions are still appearing above the results

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -3,11 +3,12 @@
 		.module("turtleFacts")
 		.controller("resultsCtrl", ResultsController);
 
-		ResultsController.$inject = ['quizMetrics'];
+		ResultsController.$inject = ['quizMetrics', 'DataService'];
 		
-		function ResultsController(quizMetrics){
+		function ResultsController(quizMetrics, DataService){
 			var vm = this;
 
 			vm.quizMetrics = quizMetrics;
+			vm.dataService = DataService;
 		}
 })();
